80d33453f9ee6cad98c3406580784b60cf88fc76
kalni <kalnichenko.1996@mail.ru>
2023-01-26 10:58:03 +0300

    example cypress test favorites

--- cypress/e2e/example.cy.ts
+++ cypress/e2e/example.cy.ts
@@ -3,6 +3,13 @@
 describe("My First Test", () => {
   it("visits the app root url", () => {
     cy.visit("/");
-    cy.contains("h1", "You did it!");
+    cy.contains("h1", "Home");
+  });
+
+  it("Add favorites item", () => {
+    cy.visit("/peoples");
+    cy.get('button').first().click();
+    cy.visit("/favorites");
+    cy.contains('tr', 'Luke Skywalker');
   });
 });
