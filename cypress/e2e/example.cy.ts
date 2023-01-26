fe6ec74b8603bf8da2cb4e235c2590c0d5864b99
kalni <kalnichenko.1996@mail.ru>
2023-01-26 11:04:39 +0300

    example cypress test Search

--- cypress/e2e/example.cy.ts
+++ cypress/e2e/example.cy.ts
@@ -1,5 +1,5 @@
 // https://docs.cypress.io/api/introduction/api.html
-
+const namePeople = 'Luke Skywalker'
 describe("My First Test", () => {
   it("visits the app root url", () => {
     cy.visit("/");
@@ -10,6 +10,12 @@ describe("My First Test", () => {
     cy.visit("/peoples");
     cy.get('button').first().click();
     cy.visit("/favorites");
-    cy.contains('tr', 'Luke Skywalker');
+    cy.contains('tr', namePeople);
+  });
+
+  it("Search", () => {
+    cy.visit("/");
+    cy.get('#search').type(namePeople)
+    cy.contains("li", namePeople);
   });
 });
